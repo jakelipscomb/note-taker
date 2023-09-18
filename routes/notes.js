428240c4e6d9aaf94e3c5fb65875847d8e6901cc
jakelipscomb <jlipscombtx95@gmail.com>
2023-09-18 15:56:53 -0500

    added fs for read and writing to api/notes for get, post, and delete calls. ability to delete notes by id. gave the test note an id so it can be deleted too

--- routes/notes.js
+++ routes/notes.js
@@ -1,29 +1,37 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const fs = require ("fs");
+const fs = require ('fs');
 
 // GET request to '/api/notes'
-router.get('/api/notes', (req, res) => {
-  const jsonData = data
+router.get('/api/notes', async (req, res) => {
+  const jsonData = await JSON.parse(fs.readFileSync('db/db.json','utf8'));
   res.json(jsonData);
 });
 
 // POST request to '/api/notes'
 router.post('/api/notes', (req, res) => {
-  const jsonData = data
+  const jsonData = JSON.parse(fs.readFileSync('db/db.json','utf8'));
   const newNote = {
     title: req.body.title,
     text: req.body.text,
     id: uuidv4(),
   };
   jsonData.push(newNote);
+  fs.writeFileSync('db/db.json',JSON.stringify(jsonData));
   res.json(jsonData);
+  console.log('Note added.')
 });
 
-
+// DELETE request to '/api/notes' by id
 router.delete('/api/notes/:id', (req, res) => {
-  const jsonData = data
-  res.json("Note deleted.");
+  let data = fs.readFileSync('db/db.json', 'utf8');
+  const jsonData =  JSON.parse(data);
+  const newNotes = jsonData.filter((note) => { 
+    return note.id !== req.params.id;
+  });
+  fs.writeFileSync('db/db.json',JSON.stringify(newNotes));
+  res.json(jsonData);
+  console.log('Note deleted.')
 });
 
 module.exports = router; 
